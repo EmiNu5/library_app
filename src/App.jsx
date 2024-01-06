78946f071e56b5a998bfd6e091027b77694e0a94
EmiNu5 <57845561+EmiNu5@users.noreply.github.com>
2024-01-05 22:49:14 -0300

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import SearchBar from './components/SearchBar'
 import TabFilter from './components/TabFilter'
 import CardBook from './components/CardBook'
 import { getLibros } from './services/book.js'
+import Navbar from './components/Navbar'
 
 const App = () => {
   const [booksStatus, setbooksStatus] = useState([])
@@ -29,6 +30,7 @@ const App = () => {
   return (
     <>
       <div className="bg-[--colorPrim] min-h-screen">
+          <Navbar/>
         <div className="py-2">
           <h1 className="font-extrabold text-4xl">Library_app</h1>
           <div className="mx-0">
