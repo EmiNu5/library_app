4dba116a5e93d1c79e069aad9072eebcca66810a
EmiNu5 <57845561+EmiNu5@users.noreply.github.com>
2024-06-01 21:33:36 -0300

    fix responsive CardBooks img

--- src/components/CardBook.jsx
+++ src/components/CardBook.jsx
@@ -2,17 +2,19 @@
 
 const CardBook = ({ allBooks, setDetailsBooks }) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-6 border-spacing-5 p-2 bg-[--colorPrim]">
+    <div 
+      className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 m-2 bg-[--colorPrim]">
       {allBooks.map((book, index) => 
         <div
           key={index}
-          className="mx-auto cursor-pointer p-2 m-2 hover:bg-[--colorSec] rounded"
+
+          className="mx-auto cursor-pointer flex items-center justify-center hover:bg-[--colorSec] rounded"
           onClick={() => setDetailsBooks(book)}
         >
           <img
             src={book.image}
             alt={book.title}
-            className="flex w-32 mx-auto h-48 object-fit rounded"
+            className= "object-fit w-32 h-48 rounded"
           />
         </div>
       )}
